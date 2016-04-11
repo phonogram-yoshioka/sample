0fc8ef720158d63f31ef072fabacb0f4ca357e8f
yoshioka <yoshioka@phonogram.co.jp>
2016-04-11 14:00:21 +0900

    no message

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>無題ドキュメント</title>
-</head>
-<body>
-修正したんですよ
-</body>
-</html>
+<?php
+
+// https://bitbucket.org/ariya/phantomjs/downloads/
+
+require __DIR__ . '/vendor/autoload.php';
+
+use JonnyW\PhantomJs\Client;
+
+$client = Client::getInstance();
+$client->getEngine()->setPath(__DIR__ . '/bin/phantomjs');
+
+$request  = $client->getMessageFactory()->createRequest();
+$response = $client->getMessageFactory()->createResponse();
+
+var_dump($request);
